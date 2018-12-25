6f79e849162caf66bdaeabfd5279b799a4aa9ce4
Pedro Blanco <pedro.blanco@outlook.com>
2018-12-25 10:22:15 +0100

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,56 @@
+#organazing list
+cars = ["bmw", "ferrari", "audi"]
+cars.sort(reverse = True)
+print(cars)
+
+cities = ["madrid", "sevilla", "new york"]
+cities.sort()
+print(cities)
+
+#working with lists
+magicians = ["alice", "david", "carolina"]
+for magician in magicians:
+	print(magician)
+
+
+#tryityourself
+
+favorite_pizzas = ["jamonyqueso", "4quesos","chorizo"]
+for pizza in favorite_pizzas:
+	print("I like " + pizza + "pizza")
+print("I really like pizza")
+
+#printing number in a for list
+
+for value in range(1,3):
+	print(value)
+
+
+squares = []
+for value in range (1, 11):
+	square = value**2
+	squares.append(square)
+print(squares)
+
+#simplystatistics
+
+numbers = [1,2,3,100,12]
+min(numbers)
+max(numbers)
+sum(numbers)
+
+
+#counting to 20
+
+for value in range (1, 21):
+	print(value)
+
+
+#counting to 1000000
+
+for value in range (1, 1000000):
+	print(value)
+
+max(value)
+
+
